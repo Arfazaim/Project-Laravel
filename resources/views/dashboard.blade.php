06d06696a0799a7b71566671ee162d5103e46fff
Arfa Zaim Al Murtadlo <arfazaim0202@gmail.com>
2024-10-31 13:28:24 +0700

    UTS

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,12 +34,6 @@
                                 <i class="ti ti-menu-2"></i>
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link nav-icon-hover" href="javascript:void(0)">
-                                <i class="ti ti-bell-ringing"></i>
-                                <div class="notification bg-primary rounded-circle"></div>
-                            </a>
-                        </li>
                     </ul>
                     <div class="navbar-collapse justify-content-end px-0" id="navbarNav">
                         <ul class="navbar-nav flex-row ms-auto align-items-center justify-content-end">
@@ -58,21 +52,13 @@
                                             <i class="ti ti-user fs-6"></i>
                                             <p class="mb-0 fs-3">My Profile</p>
                                         </a>
-                                        <a href="javascript:void(0)"
-                                           class="d-flex align-items-center gap-2 dropdown-item">
-                                            <i class="ti ti-mail fs-6"></i>
-                                            <p class="mb-0 fs-3">My Account</p>
-                                        </a>
-                                        <a href="javascript:void(0)"
-                                           class="d-flex align-items-center gap-2 dropdown-item">
-                                            <i class="ti ti-list-check fs-6"></i>
-                                            <p class="mb-0 fs-3">My Task</p>
-                                        </a>
                                         <form method="POST" action="{{ route('logout') }}">
                                             @csrf
                                             <a :href="route('logout')"
-                                                             onclick="event.preventDefault();
+                                               class="d-flex align-items-center gap-2 dropdown-item"
+                                               onclick="event.preventDefault();
                                                 this.closest('form').submit();">
+                                                <i class="ti ti-logout fs-6"></i>
                                                 {{ __('Log Out') }}
                                             </a>
                                         </form>
